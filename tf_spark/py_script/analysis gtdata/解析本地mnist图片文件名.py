acb5e23b4ba2714eae3c7f4e65d234e10609f629
fengzhongyouxia <gzcl_wcon@126.com>
2017-11-30 16:43:42 +0800

    Update 解析本地mnist图片文件名.py

--- tf_spark/py_script/analysis gtdata/解析本地mnist图片文件名.py
+++ tf_spark/py_script/analysis gtdata/解析本地mnist图片文件名.py
@@ -3,15 +3,13 @@
 
 """
 解析本地mnist图片的路径，与gtdata路径结合拼成gtdata的完整路径
-
 结果 如：
-0\0_0.bmp
-0\0_1.bmp
-0\0_10.bmp
+0/0_0.bmp
+0/0_1.bmp
+0/0_10.bmp
 ……
-8\8_189.bmp
-8\8_19.bmp
-
+8/8_189.bmp
+8/8_19.bmp
 """
 
 import glob
@@ -24,6 +22,7 @@ fp=open('mnist_name.txt','w')
 for img_path in img_paths:
     # fp.write(img_path.split('\\')[-1]) # 写入文件名
 
-    fp.write(os.path.join(img_path.split('\\')[-2],img_path.split('\\')[-1])+'\n')
+    # fp.write(os.path.join(img_path.split('\\')[-2],img_path.split('\\')[-1])+'\n')
+    fp.write(img_path.split('\\')[-2]+'/'+ img_path.split('\\')[-1] + '\n')
 
 fp.close()
