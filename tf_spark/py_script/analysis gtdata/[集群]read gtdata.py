2ba66dcf0222ae5e0e393cb368f088387853a796
fengzhongyouxia <gzcl_wcon@126.com>
2017-11-30 23:43:29 +0800

    Update [集群]read gtdata.py

--- tf_spark/py_script/analysis gtdata/[集群]read gtdata.py
+++ tf_spark/py_script/analysis gtdata/[集群]read gtdata.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-""" 集群版 从gtdata上解析数据（使用gdal读取图片数据）
+""" 集群版 从gtdata上解析数据（使用gdal读取图片数据） 使用非 one_hot 标签
 # 上传 mnist_name.txt到hdfs
 mnist_name.txt生成参考： 解析本地mnist图片文件名.py
 zip -r mnist.zip mnist_name.txt 
