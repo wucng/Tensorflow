891f73e70c90d4d30372b91f390c2693088f9f9f
fengzhongyouxia <gzcl_wcon@126.com>
2018-01-11 09:37:04 +0800

    Update merge.py

--- 图像对比检测/merge.py
+++ 图像对比检测/merge.py
@@ -124,7 +124,9 @@ def merge(file,img_save):
             # raster_fn = path.join(sys.argv[3], img_name + '_mask.tif')
 
             raster_fn = img_save + '_mask.tif'
-            papszCreateOptions = ['compress=lzw'] # 压缩
+            # papszCreateOptions = ['compress=lzw'] # 压缩
+            papszCreateOptions = 'compress=lzw'# 压缩
+            papszCreateOptions=[papszCreateOptions,"BIGTIFF", "IF_NEEDED"] # BIGTIFF 支持超过4G         
             # target_ds = gdal.GetDriverByName('GTiff').Create(raster_fn, xcount1, ycount1, 1, gdal.GDT_Byte)
             target_ds = gdal.GetDriverByName('GTiff').Create(raster_fn, int(info_struct[2]), int(info_struct[3]), 1,
                                                              gdal.GDT_Byte,papszCreateOptions)
